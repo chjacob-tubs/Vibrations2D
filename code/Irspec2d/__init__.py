071249230f58ce004b729aed2b4d3a215281a3c5
Julia Brueggemann <julia.brueggemann@tu-braunschweig.de>
2021-01-11 10:29:26 +0100

    fixed the import

--- code/Irspec2d/__init__.py
+++ code/Irspec2d/__init__.py
@@ -3,5 +3,5 @@
 #print('This code wants to evaluate 2D ir spectra.')
 #print(' '+30*'*')
 
-#from Spectrum import *
-#from Calc2dir import *
+from Irspec2d.Spectrum import *
+from Irspec2d.Calc2dir import *
